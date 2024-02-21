af14181d79c8397d7c8bedee66a8f88ad15021e3
Cameron Sivo <36455966+csiv23@users.noreply.github.com>
2024-02-21 11:19:45 -0500

    Started A3

---
+++ src/Kanbas/Navigation/index.tsx
@@ -0,0 +1,52 @@
+import { Link, useLocation } from "react-router-dom";
+import {
+  FaTachometerAlt,
+  FaRegUserCircle,
+  FaBook,
+  FaRegCalendarAlt,
+  FaInbox,
+  FaHistory,
+  FaVideo,
+  FaSignOutAlt,
+  FaQuestionCircle,
+} from "react-icons/fa";
+import { FaN } from "react-icons/fa6";
+import "./index.css";
+
+function KanbasNavigation() {
+  const links = [
+    { label: "Account", icon: <FaRegUserCircle className="fs-2 fa-user" /> },
+    {
+      label: "Dashboard",
+      icon: <FaTachometerAlt className="fs-2 wd-active" />,
+    },
+    { label: "Courses", icon: <FaBook className="fs-2" /> },
+    { label: "Calendar", icon: <FaRegCalendarAlt className="fs-2" /> },
+    { label: "Inbox", icon: <FaInbox className="fs-2" /> },
+    { label: "History", icon: <FaHistory className="fs-2" /> },
+    { label: "Studio", icon: <FaVideo className="fs-2" /> },
+    { label: "Commons", icon: <FaSignOutAlt className="fs-2" /> },
+    { label: "Help", icon: <FaQuestionCircle className="fs-2" /> },
+  ];
+
+  const { pathname } = useLocation();
+
+  return (
+    <ul className="wd-kanbas-navigation">
+      <FaN className="fs-2 fa-n" />
+      {links.map((link, index) => (
+        <li
+          key={index}
+          className={pathname.includes(link.label) ? "wd-active" : ""}
+        >
+          <Link to={`/Kanbas/${link.label}`}>
+            {" "}
+            {link.icon} {link.label}{" "}
+          </Link>
+        </li>
+      ))}
+    </ul>
+  );
+}
+
+export default KanbasNavigation;
---
+++ src/Labs/a3/JavaScript/index.tsx
@@ -0,0 +1,56 @@
+import VariablesAndConstants
+  from "./variables/VariablesAndConstants";
+import VariableTypes from "./variables/VariableTypes";
+import BooleanVariables from "./variables/BooleanVariables";
+import IfElse from "./conditionals/IfElse";
+import TernaryOperator from "./conditionals/TernaryOperator";
+import ESSFunctions from "./functions/ESSFunctions";
+import ArrowFunctions from "./functions/ArrowFunctions";
+import ImpliedReturns from "./functions/ImpliedReturns";
+import FunctionParenthesisAndParameters from "./functions/FunctionParenthesisAndParameters";
+import WorkingWithArrays from "./arrays/WorkingWithArrays";
+import ArrayIndexAndLength from "./arrays/ArrayIndexAndLength";
+import AddingAndRemovingToFromArrays from "./arrays/AddingAndRemovingDataToFromArrays";
+import ForLoops from "./arrays/ForLoops";
+import MapFunction from "./arrays/MapFunction";
+import JsonStringify from "./json/JsonStringify";
+import FindFunction from "./arrays/FindFunction";
+import FindIndex from "./arrays/FindIndex";
+import FilterFunction from "./arrays/FilterFunction";
+import TemplateLiterals from "./string/TemplateLiterals";
+import House from "./json/House";
+import Spreading from "./json/Spreading";
+import Destructing from "./json/Destructing";
+import FunctionDestructing from "./functions/FunctionDestructing";
+function JavaScript() {
+    console.log('Hello World!');
+    return(
+       <div>
+         <h1>JavaScript</h1>
+         <VariablesAndConstants/>
+         <VariableTypes/>
+         <BooleanVariables/>
+         <IfElse/>
+         <TernaryOperator/>
+         <ESSFunctions/>
+         <ArrowFunctions/>
+         <ImpliedReturns/>
+         <FunctionParenthesisAndParameters/>
+         <WorkingWithArrays/>
+         <ArrayIndexAndLength/>
+         <AddingAndRemovingToFromArrays/>
+         <ForLoops/>
+         <MapFunction/>
+         <JsonStringify/>
+         <FindFunction/>
+         <FindIndex/>
+         <FilterFunction/>
+         <TemplateLiterals/>
+         <House/>
+         <Spreading/>
+         <Destructing/>
+         <FunctionDestructing/>
+       </div>
+    );
+ }
+ export default JavaScript;
