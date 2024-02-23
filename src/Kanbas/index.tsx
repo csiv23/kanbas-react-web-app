2da1467d6f8695f53b345dba1e76cbb47fc869ae
Cameron Sivo <36455966+csiv23@users.noreply.github.com>
2024-02-23 16:00:10 -0500

    Added and styled courses screen header

--- src/Kanbas/index.tsx
+++ src/Kanbas/index.tsx
@@ -1,4 +1,5 @@
 import KanbasNavigation from "./Navigation";
+import Courses from "./Courses";
 import { Routes, Route, Navigate } from "react-router-dom";
 import Dashboard from "./Dashboard";
 function Kanbas() {
@@ -10,7 +11,7 @@ function Kanbas() {
           <Route path="/" element={<Navigate to="Dashboard" />} />
           <Route path="Account" element={<h1>Account</h1>} />
           <Route path="Dashboard" element={<Dashboard />} />
-          <Route path="Courses/*" element={<h1>Courses</h1>} />
+          <Route path="Courses/:courseId/*" element={<Courses />} />
         </Routes>
       </div>
     </div>
