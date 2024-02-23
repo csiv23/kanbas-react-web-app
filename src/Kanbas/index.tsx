c75810a84f9ab59cb157343b2efea5a5b0cb0d74
Cameron Sivo <36455966+csiv23@users.noreply.github.com>
2024-02-23 18:41:06 -0500

    added further styling

--- src/Kanbas/index.tsx
+++ src/Kanbas/index.tsx
@@ -2,6 +2,7 @@ import KanbasNavigation from "./Navigation";
 import Courses from "./Courses";
 import { Routes, Route, Navigate } from "react-router-dom";
 import Dashboard from "./Dashboard";
+
 function Kanbas() {
   return (
     <div className="d-flex">
@@ -11,10 +12,14 @@ function Kanbas() {
           <Route path="/" element={<Navigate to="Dashboard" />} />
           <Route path="Account" element={<h1>Account</h1>} />
           <Route path="Dashboard" element={<Dashboard />} />
-          <Route path="Courses/:courseId/*" element={<Courses />} />
+          <Route path="Courses/:courseId" element={<Courses />}>
+            <Route index element={<Navigate to="Home" replace />} />
+            <Route path=":section/*" element={<Courses />} />
+          </Route>
         </Routes>
       </div>
     </div>
   );
 }
+
 export default Kanbas;
