52c27b36efc977c0ff7349e317e21b8701922900
Cameron Sivo <36455966+csiv23@users.noreply.github.com>
2024-03-19 13:08:11 -0400

    completed lab

--- src/Labs/a4/index.tsx
+++ src/Labs/a4/index.tsx
@@ -1,9 +1,34 @@
 import React from "react";
-
+import ClickEvent from "./ClickEvent";
+import PassingDataOnEvent from "./PassingDataOnEvent";
+import PassingFunctions from "./PassingFunctions";
+import Counter from "./Counter";
+import BooleanStateVariables from "./BooleanStateVariables";
+import StringStateVariables from "./StringStateVariables.";
+import DateStateVariable from "./DateStateVariable";
+import ObjectStateVariable from "./ObjectStateVariable";
+import ArrayStateVariable from "./ArrayStateVariable";
+import ParentStateComponent from "./ParentStateComponent";
+import ReduxExamples from "./ReduxExamples";
 const Assignment4 = () => {
-  return(
+  function sayHello() {
+    alert("Hello");
+  }
+
+  return (
     <>
       <h1>Assignment 4</h1>
+      <ClickEvent></ClickEvent>
+      <PassingDataOnEvent></PassingDataOnEvent>
+      <PassingFunctions theFunction={sayHello}></PassingFunctions>
+      <Counter></Counter>
+      <BooleanStateVariables></BooleanStateVariables>
+      <StringStateVariables></StringStateVariables>
+      <DateStateVariable></DateStateVariable>
+      <ObjectStateVariable></ObjectStateVariable>
+      <ArrayStateVariable></ArrayStateVariable>
+      <ParentStateComponent></ParentStateComponent>
+      <ReduxExamples></ReduxExamples>
     </>
   );
 };
