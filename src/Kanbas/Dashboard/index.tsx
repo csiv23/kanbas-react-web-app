ff53aeb66a2d90e8923eb541bd8dd60c16d13cd4
Cameron Sivo <36455966+csiv23@users.noreply.github.com>
2024-03-19 13:43:25 -0400

    3.1 dashboard

--- src/Kanbas/Dashboard/index.tsx
+++ src/Kanbas/Dashboard/index.tsx
@@ -1,16 +1,90 @@
-import React from "react";
+import React, { useState } from "react";
 import { Link } from "react-router-dom";
-import { courses } from "../Database";
-
+import db from "../Database";
 function Dashboard() {
+  const [courses, setCourses] = useState(db.courses);
+  const [course, setCourse] = useState({
+    _id: "0",
+    name: "New Course",
+    number: "New Number",
+    image: "/images/reactjs.jpg",
+    subtitle: "New Course Subtitle",
+    term: "2023 Fall Term",
+  });
+  const addNewCourse = () => {
+    const newCourse = {
+      ...course,
+      _id: new Date().getTime().toString(),
+      image: course.image || "reactjs.jpg",
+    };
+    setCourses([...courses, { ...course, ...newCourse }]);
+  };
+  const deleteCourse = (courseId: string) => {
+    setCourses(courses.filter((course) => course._id !== courseId));
+  };
+
+  const updateCourse = () => {
+    setCourses(
+      courses.map((c) => {
+        if (c._id === course._id) {
+          return course;
+        } else {
+          return c;
+        }
+      })
+    );
+  };
+
   return (
     <div className="p-4">
-      <h1>Dashboard</h1> <hr />
+      <h5>Course</h5>
+      <input
+        value={course.name}
+        className="form-control"
+        onChange={(e) => setCourse({ ...course, name: e.target.value })}
+      />
+      <input
+        value={course.number}
+        className="form-control"
+        onChange={(e) => setCourse({ ...course, number: e.target.value })}
+      />
+      <input
+        value={course.subtitle}
+        className="form-control"
+        onChange={(e) => setCourse({ ...course, subtitle: e.target.value })}
+      />
+      <input
+        value={course.term}
+        className="form-control"
+        onChange={(e) => setCourse({ ...course, term: e.target.value })}
+      />
+      <button onClick={addNewCourse}>Add</button>
+      <button onClick={updateCourse}>Update</button>
       <h2>Published Courses ({courses.length})</h2> <hr />
       <div className="row">
         <div className="row row-cols-1 row-cols-md-5 g-4">
           {courses.map((course) => (
             <div key={course._id} className="col" style={{ width: "300px" }}>
+              <button
+                onClick={(event) => {
+                  event.preventDefault();
+                  setCourse({
+                    ...course,
+                    image: course.image || "defaultImagePath.jpg",
+                  });
+                }}
+              >
+                Edit
+              </button>
+              <button
+                onClick={(event) => {
+                  event.preventDefault();
+                  deleteCourse(course._id);
+                }}
+              >
+                Delete
+              </button>
+
               <div className="card" style={{ height: "275px" }}>
                 <img
                   src={`/images/${course.image}`}
@@ -30,6 +104,7 @@ function Dashboard() {
                   >
                     {course.name}
                   </Link>
+
                   <p className="card-text">{course.name}</p>
                   <Link
                     to={`/Kanbas/Courses/${course._id}/Home`}
--- src/Kanbas/Database/index.ts
+++ src/Kanbas/Database/index.ts
@@ -1,4 +1,43 @@
 import courses from "./courses.json";
 import modules from "./modules.json";
 import assignments from "./assignments.json";
-export { courses, modules, assignments };
+
+export type CourseType = {
+  _id: string;
+  name: string;
+  subtitle: string;
+  term: string;
+  number: string;
+  image?: string | undefined;
+};
+
+export type ModuleLesson = {
+  _id: string;
+  name: string;
+  description: string;
+  module: string;
+};
+
+export type ModuleType = {
+  _id: string;
+  name: string;
+  description: string;
+  course: string;
+  lessons?: ModuleLesson[] | undefined;
+};
+
+export interface DB {
+  courses: CourseType[];
+  modules: object;
+  assignments: object;
+}
+
+const db: DB = {
+  courses: courses,
+  modules: modules,
+  assignments: assignments,
+};
+
+export default db;
+
+export { courses, assignments, modules };
